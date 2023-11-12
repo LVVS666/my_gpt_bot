04545d2807fb779b8bd08a28cfff9882e796d5cc
Василий Логинов <97108615+LVVS666@users.noreply.github.com>
2023-11-12 14:22:51 -0800

    new_log

--- bot_gpt_tegram.py
+++ bot_gpt_tegram.py
@@ -26,28 +26,29 @@ dialog_history = {}
 
 @dp.message_handler(content_types= types.ContentType.VOICE)
 async def handle_message(message: types.Message):
-    # global dialog_history
-    # user_id = message.from_user.id
-    # user_dialog_history = dialog_history.get(user_id, [])
-    # user_dialog_history.append(message.text)
-    # full_dialog = '\n'.join(user_dialog_history)
-    # logging.info(f'{message.from_user}: {message.text}')
-    # chat = openai.ChatCompletion.create(
-    #     model="gpt-3.5-turbo",
-    #     messages=[
-    #         {"role": "system", "content": "You are a bot"},
-    #         {"role": "user", "content": full_dialog}
-    #     ]
-    # )
-    # user_dialog_history.append(chat.choices[0].message.content)
-    # dialog_history[user_id] = user_dialog_history
-    # reply = chat.choices[0].message.content
-    # logging.info(f'{reply}')
+    global dialog_history
     file_voice = await bot.get_file(message.voice.file_id)
     file_path = file_voice.file_path
     download_file = await bot.download_file(file_path)
     convert_text = convert_audio_in_text.convert_text(download_file)
-    await message.reply(convert_text)
+    user_id = message.from_user.id
+    user_dialog_history = dialog_history.get(user_id, [])
+    user_dialog_history.append(convert_text)
+    full_dialog = '\n'.join(user_dialog_history)
+    logging.info(f'{message.from_user}: {message.text}')
+    chat = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": "You are a bot"},
+            {"role": "user", "content": full_dialog}
+        ]
+    )
+    user_dialog_history.append(chat.choices[0].message.content)
+    dialog_history[user_id] = user_dialog_history
+    reply = chat.choices[0].message.content
+    logging.info(f'{reply}')
+
+    await message.reply(reply)
 
 if __name__ == '__main__':
     executor.start_polling(dp, skip_updates=True)
